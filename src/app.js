b841cfaa561d90d49ce951b5ec035de5a4f091f8
melindalegg <129700945+melindalegg@users.noreply.github.com>
2023-05-28 22:44:56 +1200

    Added new search-form and functionality
    Added rows for search form and added o the functions for searching a city

--- src/app.js
+++ src/app.js
@@ -43,8 +43,18 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", `response.data.weather[0].description`);
 }
 
-let apiKey = "1b154ee88947052f4b2babf8dbba26e6";
-let city = "Auckland";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "1b154ee88947052f4b2babf8dbba26e6";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-search");
+  search(cityInputElement.value);
+}
+search("Auckland");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
